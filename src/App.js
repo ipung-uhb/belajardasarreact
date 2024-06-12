e3476eb947fc87fdfdb4d8f3dcce2fa30e06fbc8
ipung-uhb <purwono@uhb.ac.id>
2024-06-12 15:19:36 +0700

    implementasi props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Blog from './Functional/Blog';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Blog nama="Joko"/>
+        <Blog nama="Indah"/>
+        <Blog nama="Sari"/>
     </div>
   );
 }
---
+++ src/Functional/Blog.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Blog = (props) => {
+    return(
+        <div>Hello {props.nama}</div>
+    )
+}
+
+export default Blog
